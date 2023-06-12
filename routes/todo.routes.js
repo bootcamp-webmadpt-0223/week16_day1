a8db20f52437ba5ce12e4721d72f3ee881ef6a6f
Diego Méndez <diegomen15@gmail.com>
2023-06-12 20:28:34 +0200

    feat: Controller version

--- routes/todo.routes.js
+++ routes/todo.routes.js
@@ -1,59 +1,19 @@
 const router = require("express").Router()
 const Todo = require("../models/Todo.model");
-
-// TODO: CRUD
+const todoController = require("../controllers/todo.controller");
 
 // C(R)UD
-router.get("/", async (req, res, next) => {
-  try {
-    const todos = await Todo.find();
-    return res.status(200).json(todos);
-  } catch (error) {
-    next(error);
-  }
-})
+router.get("/", todoController.list);
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const todo = await Todo.findById(id);
-    return res.status(200).json(todo);
-  } catch (error) {
-    next(error);
-  }
-})
+router.get("/:id", todoController.detail);
 
 // (C)RUD
-router.post("/", async (req, res, next) => {
-  try {
-    if(!req.body.title) return res.status(400).json({ message: "Bad request" });
-    const todo = await Todo.create(req.body);
-    return res.status(201).json(todo);
-  } catch (error) {
-    next(error);
-  }
-})
+router.post("/", todoController.create);
 
 // CR(U)D
-router.put("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const todo = await Todo.findByIdAndUpdate(id, req.body, { new: true });
-    return res.status(200).json(todo);
-  } catch (error) {
-    next(error);
-  }
-})
+router.put("/:id", todoController.update);
 
 // CRU(D)
-router.delete("/:id", async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    await Todo.findByIdAndDelete(id);
-    return res.status(200).json({ message: `Todo ${id} successfully deleted 🗑`});
-  } catch (error) {
-    next(error);
-  }
-})
+router.delete("/:id", todoController.delete);
 
 module.exports = router;
